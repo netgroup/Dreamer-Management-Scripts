473ae9a7bee3f272b2c6e61dcc7f0f7daeae7838
Siracusano <giu.siracusano@gmail>
2014-05-28 14:34:11 +0200

    othernodes no ip on  vxlan bridge

--- othernodes/config.sh
+++ othernodes/config.sh
@@ -13,6 +13,70 @@ echo "#############################################################"
 TUNL_BRIDGE=br-tun
 VXLAN=yes
 
+
+plain_node_vxlan2 () {
+        
+        ii=0
+        
+        create_vxlan_interfaces
+        ovs-vsctl add-br $TUNL_BRIDGE
+        echo -e "\n-Adding interfaces to bridge $TUNL_BRIDGE"
+        
+        #TODO: solo le tap che stanno su quella eth 
+        y=0
+        for i in ${TAP[@]}; do
+                eval remoteport=\${${i}[1]}
+                eval remoteaddr=\${!$i[3]}
+                ovs-vsctl add-port $TUNL_BRIDGE  $i -- set Interface $i type=vxlan options:remote_ip=$remoteaddr options:dst_port=$remoteport
+                ovs-vsctl add-port $TUNL_BRIDGE vi-$i -- set Interface vi-$i type=internal
+                eval ip=\${${TAP[$y]}[2]}
+                echo $ip
+                ip a a $ip  dev vi-$i
+                y=$((y+1))
+        done
+        declare -a port_tap &&
+        declare -a port_vi_tap &&
+        for i in ${TAP[@]}; do
+            PORT_TAP[${#PORT_TAP[@]}]=$(ovs-vsctl find Interface name=$i | grep -m 1 ofport | awk -F':' '{print $2}' | awk '{ gsub (" ", "", $0); print}')
+            PORT_VI_TAP[${#PORT_VI_TAP[@]}]=$(ovs-vsctl find Interface name=vi-$i | grep -m 1 ofport | awk -F':' '{print $2}' | awk '{ gsub (" ", "", $0); print}')
+        done
+        
+        for (( i=0; i<${#PORT_TAP[@]}; i++ )); do
+                ovs-ofctl add-flow $TUNL_BRIDGE hard_timeout=0,priority=300,in_port=${PORT_TAP[$i]},action=output:${PORT_VI_TAP[$i]}
+                ovs-ofctl add-flow $TUNL_BRIDGE hard_timeout=0,priority=300,in_port=${PORT_VI_TAP[$i]},action=output:${PORT_TAP[$i]}
+        done
+        ii=$((ii+1))
+}
+
+create_vxlan_interfaces () {
+        ii=0
+        for j in ${INTERFACES[@]}; do
+                echo -e "\n-Creating OpenVSwitch bridge $TUNL_BRIDGE"
+                eval interface_ip=\${${j}[0]}
+                eval interface_netmask=\${${j}[1]}
+                ip link set ${INTERFACES[$ii]} up
+                vconfig add ${INTERFACES[$ii]} $SLICEVLAN
+                ip link set ${INTERFACES[$ii]}.$SLICEVLAN up
+                ifconfig $j.$SLICEVLAN $interface_ip netmask $interface_netmask
+                #ip r d 192.168.0.0/16 dev $j.$SLICEVLAN  # perche la cancellavo?                
+                ii=$((ii+1))
+        done
+        # set static routes
+    declare -a ENDIPS
+    for i in ${TAP[@]}; do
+            eval ELEMENT=\${${i}[3]}
+            if [ $(echo ${ENDIPS[@]} | grep -o $ELEMENT | wc -w) -eq 0 ];then
+                    ENDIPS[${#ENDIPS[@]}]=$ELEMENT
+            fi
+    done
+    for (( i=0; i<${#ENDIPS[@]}; i++ )); do
+            eval remoteaddr=\${${ENDIPS[$i]}[0]}
+            eval interface=\${${ENDIPS[$i]}[1]}
+            ip r a $remoteaddr dev $interface.$SLICEVLAN 
+    done
+}
+
+
 plain_node_vxlan () {
         
         ii=0
